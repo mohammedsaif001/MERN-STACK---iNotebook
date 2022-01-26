4e5305785666fbb86cbf4a19d063a3f0fcca2d39
Mohammed Saif <mdsaif4online@gmail.com>
2022-01-26 22:41:43 +0530

    7 Hashing Password using bcryptJs, adding salt, using json webtoken for authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'VillainIsTheBest';
 
 // Checking Validation using express-validator
 //Create a USer Using POST : '/api/auth/createuser' -> No LOgin Require
@@ -21,19 +25,34 @@ router.post(
             return res.status(400).json({ errors: errors.array() });
         }
 
-        //Check Whether the user with this email exists already
         try {
+            //Check Whether the user with this email exists already
             let user = await User.findOne({ email: req.body.email });
             if (user) {
                 return res.status(400).json({ error: "Sorry a User with Email ALready Exists" });
             }
+
+            const salt = await bcrypt.genSalt(10); //Adding Salt after HAsing
+            const secPass =await bcrypt.hash(req.body.password,salt)  //Hashing the password and adding salt to it
+
             //Creating a New User using Express-Validation
             user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password,
+                password:secPass,
             });
-            res.json(user); //Displaying record in response body
+            
+            //Generating Token using JWT
+            const data = {
+                user :{
+                    id : user.id
+                }
+            }
+            const authToken = jwt.sign(data,JWT_SECRET);
+            
+
+            res.json({authToken}); //Generating token for client
+            // res.json(user);// Displaying record in response body
         } catch (error) {
             console.error(error.mesaage);
             res.status(500).send("Some Error Occured");
