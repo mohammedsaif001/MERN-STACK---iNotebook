a64d79da31695cc0e95942792f07108ec7fdac38
Mohammed Saif <mdsaif4online@gmail.com>
2022-01-27 20:52:56 +0530

    13 Setting up react router and adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './Component/Navbar';
+import Home from './Component/Home';
+import About from './Component/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNoteBook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+      
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
