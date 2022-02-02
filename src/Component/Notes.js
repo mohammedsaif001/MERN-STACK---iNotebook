662d5767be9fd4104037f93ea881defa29ae85ab
Mohammed Saif <mdsaif4online@gmail.com>
2022-02-03 00:10:59 +0530

    23 Updating notes on Edit in the UI

--- src/Component/Notes.js
+++ src/Component/Notes.js
@@ -5,22 +5,23 @@ import Noteitem from './Noteitem';
 
 export default function Notes() {
   const context = useContext(noteContext);
-  const { notes, getNotes } = context;
+  const { notes, getNotes, editNote } = context;
   useEffect(() => {
     getNotes()
 
   }, []);
   const ref = useRef(null)
-  const [note, setNote] = useState({ etitle: "", edescription: "", etag: "" })
+  const refClose = useRef(null)
+  const [note, setNote] = useState({ id: "", etitle: "", edescription: "", etag: "" })
 
   const updateNote = (currentNote) => {
     ref.current.click();
-    setNote({ etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag })
+    setNote({ id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag })
   }
 
   const handleClick = (e) => {
-    console.log("Updating the note...", note)
-    e.preventDefault();
+    editNote(note.id, note.etitle, note.edescription, note.etag)
+    refClose.current.click();
   }
 
   const onChange = (e) => {
@@ -57,8 +58,8 @@ export default function Notes() {
 
             </form>
           </div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <div className="modal-footer">   
+             <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             <button onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
           </div>
         </div>
