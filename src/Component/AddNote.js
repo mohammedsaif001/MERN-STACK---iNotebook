51a04b38e054f519baf50d8620599ba542d0a069
Mohammed Saif <mdsaif4online@gmail.com>
2022-02-03 19:39:51 +0530

    24 Adding FrontEnd validation

--- src/Component/AddNote.js
+++ src/Component/AddNote.js
@@ -5,11 +5,12 @@ import noteContext from '../Context/Notes/noteContext';
 export default function AddNote() {
     const context = useContext(noteContext);
     const {addNote } = context;
-    const [note, setnote] = useState({title:"",description:"",tag:"default"});
+    const [note, setnote] = useState({title:"",description:"",tag:""});
 
     const handleClick = (e)=>{
         e.preventDefault()
         addNote(note.title,note.description,note.tag);
+        setnote({title:"",description:"",tag:""})
     }
     const onChange = (e)=>{
         setnote({...note,[e.target.name]:e.target.value}) //Change the name of title and description to the given value
@@ -20,17 +21,17 @@ export default function AddNote() {
         <form>
             <div className="mb-3">
                 <label htmlFor="title" className="form-label">Title</label>
-                <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" onChange={onChange} />
+                <input type="text" className="form-control" id="title" value={note.title} name="title" aria-describedby="emailHelp" onChange={onChange} minLength={5} required/>
             </div>
             <div className="mb-3">
                 <label htmlFor="description" className="form-label">Description</label>
-                <input type="text" className="form-control" id="description " onChange={onChange} name='description' />
+                <input type="text"  value={note.description}className="form-control" id="description " onChange={onChange} name='description' minLength={5} required />
             </div>
             <div className="mb-3">
                     <label htmlFor="tag" className="form-label">Tag</label>
-                    <input type="text" className="form-control" id="tag" name="tag" onChange={onChange} />
+                    <input type="text" value={note.tag} className="form-control" id="tag" name="tag" onChange={onChange} minLength={5} required/>
             </div>
-            <button type="submit" className="btn btn-primary" onClick={handleClick}>ADD NOTE</button>
+            <button disabled={note.title.length<5 || note.description.length<5} type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button>
         </form>
     </div>;
 }
--- src/Context/Notes/NoteState.js
+++ src/Context/Notes/NoteState.js
@@ -17,7 +17,6 @@ const NoteState = (props) => {
             }
         });
         const json = await response.json()
-        console.log(json)
         setNotes(json)
     }
 
@@ -29,26 +28,14 @@ const NoteState = (props) => {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjEzMWRjNWUzZTQwMzdjZDQ3MzRhMDY2In0sImlhdCI6MTYzMDY2OTU5Nn0.hJS0hx6I7ROugkqjL2CjrJuefA3pJi-IU5yGUbRHI4Q"
+                "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjFmMTgwNjFhYmZkMDcxYjMzOWFiNzU0In0sImlhdCI6MTY0MzI2NTY3OX0.r65Uw8sGpFT2aR731wv9-5Rrhigy4315Jq31hGbXIRQ"
             },
             body: JSON.stringify({ title, description, tag })
 
         });
-        const json = await response.json();
-        console.log(json)
-
-
-        console.log("Adding a new note")
-        const note = {
-            "_id": "61322f119553781a8ca8d0e08",
-            "user": "6131dc5e3e4037cd4734a0664",
-            "title": title,
-            "description": description,
-            "tag": tag,
-            "date": "2021-09-03T14:20:09.668Z",
-            "__v": 0
-        };
+        const note = await response.json();
         setNotes(notes.concat(note))
+       
     }
 
     // Delete a Note
@@ -59,12 +46,11 @@ const NoteState = (props) => {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
-                "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjEzMWRjNWUzZTQwMzdjZDQ3MzRhMDY2In0sImlhdCI6MTYzMDY2OTU5Nn0.hJS0hx6I7ROugkqjL2CjrJuefA3pJi-IU5yGUbRHI4Q"
+                "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjFmMTgwNjFhYmZkMDcxYjMzOWFiNzU0In0sImlhdCI6MTY0MzI2NTY3OX0.r65Uw8sGpFT2aR731wv9-5Rrhigy4315Jq31hGbXIRQ"
             }
         });
         const json = response.json();
-        console.log(json)
-        console.log("Deleting the note with id" + id);
+
         const newNotes = notes.filter((note) => { return note._id !== id })
         setNotes(newNotes)
     }
@@ -80,7 +66,6 @@ const NoteState = (props) => {
             body: JSON.stringify({ title, description, tag })
         });
         const json = await response.json();
-        console.log(json)
 
         let newNotes = JSON.parse(JSON.stringify(notes))
         // Logic to edit in client
